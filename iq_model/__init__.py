7f7206756efa9c36701323864a5b9bf43ece5b7e
mikrós <mddlecloudy@gmail.com>
2026-09-14 18:53:40 -0400

    Export IQ research backbone

---
+++ iq_model/__init__.py
@@ -0,0 +1,8 @@
+from .config import IQArchitectureConfig
+from .model import IQOutput, IQRecurrentPhiModel
+
+__all__ = [
+    "IQArchitectureConfig",
+    "IQOutput",
+    "IQRecurrentPhiModel",
+]
