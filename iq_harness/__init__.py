1c9312ee04dbfdbe7e9c51ef50554c4dbbab8fed
mikrós <mddlecloudy@gmail.com>
2026-09-14 16:26:02 -0400

    Expose IQ agent harness API

---
+++ iq_harness/__init__.py
@@ -0,0 +1,31 @@
+from .model import ModelBackend
+from .runtime import AgentRuntime, AgentRuntimeError
+from .session import InMemorySession, JsonlSession, Session
+from .skills import Skill, SkillCatalog, SkillError, load_skill
+from .tools import Tool, ToolError, ToolProvider, ToolRegistry
+from .types import AgentSpec, Message, ModelRequest, ModelTurn, RunResult, ToolCall, ToolDescriptor, TraceEvent
+
+__all__ = [
+    "AgentRuntime",
+    "AgentRuntimeError",
+    "AgentSpec",
+    "InMemorySession",
+    "JsonlSession",
+    "Message",
+    "ModelBackend",
+    "ModelRequest",
+    "ModelTurn",
+    "RunResult",
+    "Session",
+    "Skill",
+    "SkillCatalog",
+    "SkillError",
+    "Tool",
+    "ToolCall",
+    "ToolDescriptor",
+    "ToolError",
+    "ToolProvider",
+    "ToolRegistry",
+    "TraceEvent",
+    "load_skill",
+]
