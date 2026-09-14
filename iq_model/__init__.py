9cdf103b579b1ce1bd6be6ee8d8661ce8a05ee46
mikrós <mddlecloudy@gmail.com>
2026-09-14 18:56:59 -0400

    Export recurrent architecture and transfer layout

--- iq_model/__init__.py
+++ iq_model/__init__.py
@@ -1,8 +1,14 @@
 from .config import IQArchitectureConfig
 from .model import IQOutput, IQRecurrentPhiModel
+from .reasoning import RecurrentCoreOutput, RecurrentReasoningCore
+from .transfer_layout import DenseToRecurrentLayout, SharedCoreTarget
 
 __all__ = [
+    "DenseToRecurrentLayout",
     "IQArchitectureConfig",
     "IQOutput",
     "IQRecurrentPhiModel",
+    "RecurrentCoreOutput",
+    "RecurrentReasoningCore",
+    "SharedCoreTarget",
 ]
