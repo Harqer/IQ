0863b2433df2fb6768e946520bf8a5af52ad3f45
mikrós <mddlecloudy@gmail.com>
2026-09-14 19:19:57 -0400

    Export hybrid architecture utilities

--- iq_model/__init__.py
+++ iq_model/__init__.py
@@ -1,4 +1,10 @@
 from .config import IQArchitectureConfig
+from .keystone import KeystoneActivationMonitor, KeystoneActivationSummary
+from .mixers import (
+    MixerContext,
+    MixerDependencyError,
+    UnsupportedMixerComposition,
+)
 from .model import IQOutput, IQRecurrentPhiModel
 from .reasoning import RecurrentCoreOutput, RecurrentReasoningCore
 from .transfer_layout import DenseToRecurrentLayout, SharedCoreTarget
@@ -8,7 +14,12 @@ __all__ = [
     "IQArchitectureConfig",
     "IQOutput",
     "IQRecurrentPhiModel",
+    "KeystoneActivationMonitor",
+    "KeystoneActivationSummary",
+    "MixerContext",
+    "MixerDependencyError",
     "RecurrentCoreOutput",
     "RecurrentReasoningCore",
     "SharedCoreTarget",
+    "UnsupportedMixerComposition",
 ]
