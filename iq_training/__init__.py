411ee6124bcee6d5921866d1bbf22ac2cdcd30f2
mikrós <mddlecloudy@gmail.com>
2026-09-18 20:15:30 -0700

    Add dense recipient training support: iq_training/__init__.py

---
+++ iq_training/__init__.py
@@ -0,0 +1,3 @@
+from .checkpoint import CheckpointError, CheckpointMetadata, load_checkpoint, save_checkpoint
+
+__all__ = ["CheckpointError", "CheckpointMetadata", "load_checkpoint", "save_checkpoint"]
