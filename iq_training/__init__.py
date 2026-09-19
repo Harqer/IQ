233c2064b14f663a925ac2118a888b52c32c7265
mikrós <mddlecloudy@gmail.com>
2026-09-18 20:19:40 -0700

    Export IQ production training APIs

--- iq_training/__init__.py
+++ iq_training/__init__.py
@@ -1,3 +1,20 @@
 from .checkpoint import CheckpointError, CheckpointMetadata, load_checkpoint, save_checkpoint
+from .optimizer import IQOptimizer, OptimizerConfig, OptimizerConfigError, OptimizerCoverage, build_optimizer, classify_parameters
+from .train import TrainingError, TrainStepConfig, TrainStepMetrics, train_step
 
-__all__ = ["CheckpointError", "CheckpointMetadata", "load_checkpoint", "save_checkpoint"]
+__all__ = [
+    "CheckpointError",
+    "CheckpointMetadata",
+    "IQOptimizer",
+    "OptimizerConfig",
+    "OptimizerConfigError",
+    "OptimizerCoverage",
+    "TrainStepConfig",
+    "TrainStepMetrics",
+    "TrainingError",
+    "build_optimizer",
+    "classify_parameters",
+    "load_checkpoint",
+    "save_checkpoint",
+    "train_step",
+]
