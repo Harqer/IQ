8385291a3f0c6a9be48167026e876abc81631c90
mikrós <mddlecloudy@gmail.com>
2026-09-24 05:23:07 -0400

    Export routed SwiGLU MoE APIs

--- iq_model/mlp/__init__.py
+++ iq_model/mlp/__init__.py
@@ -1,3 +1,17 @@
+from .moe import (
+    MoEConfigError,
+    MoEOutput,
+    RoutedMoEConfig,
+    RoutedSwiGLUMoE,
+    SwiGLUExpert,
+)
 from .swiglu import SwiGLU
 
-__all__ = ["SwiGLU"]
+__all__ = [
+    "MoEConfigError",
+    "MoEOutput",
+    "RoutedMoEConfig",
+    "RoutedSwiGLUMoE",
+    "SwiGLU",
+    "SwiGLUExpert",
+]
