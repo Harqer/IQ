3fa1e0dbda067afdf7256f579c88498e68a2d971
mikrós <mddlecloudy@gmail.com>
2026-09-24 19:00:19 -0400

    Add mHC residual package

---
+++ iq_model/residual/__init__.py
@@ -0,0 +1,19 @@
+from .mhc import (
+    MHCConfig,
+    MHCError,
+    MHCHead,
+    MHCWeights,
+    ManifoldHyperConnection,
+    expand_mhc_streams,
+    sinkhorn_doubly_stochastic,
+)
+
+__all__ = [
+    "MHCConfig",
+    "MHCError",
+    "MHCHead",
+    "MHCWeights",
+    "ManifoldHyperConnection",
+    "expand_mhc_streams",
+    "sinkhorn_doubly_stochastic",
+]
