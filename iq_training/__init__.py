dac90541ff5b28adb5eaf9bf4b89456c68fb3539
mikrós <mddlecloudy@gmail.com>
2026-09-26 07:10:00 -0400

    Export reasoning EBM training APIs

--- iq_training/__init__.py
+++ iq_training/__init__.py
@@ -1,3 +1,4 @@
+from .reasoning_critic import ReasoningCriticPairingConfig, ReasoningCriticStepMetrics, ReasoningCriticTrainConfig, ReasoningCriticTrainingError, ReasoningEnergyPairBatch, ReasoningTrajectoryBatch, build_same_task_energy_pairs, reasoning_critic_pair_loss, train_reasoning_critic_step
 from .pretraining import IQPretrainingModel, PretrainingConfigError, PretrainingObjectiveConfig, PretrainingOutput
 from .checkpoint import CheckpointError, CheckpointMetadata, load_checkpoint, save_checkpoint
 from .optimizer import IQOptimizer, OptimizerConfig, OptimizerConfigError, OptimizerCoverage, build_optimizer, classify_parameters
@@ -12,14 +13,23 @@ __all__ = [
     "PretrainingConfigError",
     "PretrainingObjectiveConfig",
     "PretrainingOutput",
+    "ReasoningCriticPairingConfig",
+    "ReasoningCriticStepMetrics",
+    "ReasoningCriticTrainConfig",
+    "ReasoningCriticTrainingError",
+    "ReasoningEnergyPairBatch",
+    "ReasoningTrajectoryBatch",
     "OptimizerConfigError",
     "OptimizerCoverage",
     "TrainStepConfig",
     "TrainStepMetrics",
     "TrainingError",
     "build_optimizer",
+    "build_same_task_energy_pairs",
     "classify_parameters",
     "load_checkpoint",
+    "reasoning_critic_pair_loss",
     "save_checkpoint",
+    "train_reasoning_critic_step",
     "train_step",
 ]
