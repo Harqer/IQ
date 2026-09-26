f2517b07dfa5bce27a2fed76a683a76ca220d432
mikrós <mddlecloudy@gmail.com>
2026-09-26 06:20:33 -0400

    Add standalone reasoning energy critic API

---
+++ iq_model/energy/__init__.py
@@ -0,0 +1,11 @@
+from .critic import (
+    ReasoningEnergyCritic,
+    ReasoningEnergyCriticConfig,
+    energy_margin_ranking_loss,
+)
+
+__all__ = [
+    "ReasoningEnergyCritic",
+    "ReasoningEnergyCriticConfig",
+    "energy_margin_ranking_loss",
+]
